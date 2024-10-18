ca9e5c41f383914db7714d6fe6de9824f128636a
Casey Edwards <cjedwards233@yahoo.com>
2024-10-18 13:23:23 -0500

    adds CLC support

--- Program.cs
+++ Program.cs
@@ -1,23 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Azure;
 using revisa_api.Data.content;
 using revisa_api.Data.elps;
 using revisa_api.Data.language_supports;
 using revisa_api.Data.teks;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// db setup and registration
-string connectionString =
+// Azure services registration
+string? blobConnectionString =
+    Environment.GetEnvironmentVariable("REVISA_BUCKET")
+    ?? builder.Configuration.GetConnectionString("REVISA_BUCKET");
+
+builder.Services.AddAzureClients(clientBuilder =>
+{
+    clientBuilder.AddBlobServiceClient(blobConnectionString);
+});
+
+// Database config setup
+string? connectionString =
     Environment.GetEnvironmentVariable("REVISA_DB")
     ?? builder.Configuration.GetConnectionString("REVISA_DB");
+
 Action<DbContextOptionsBuilder> dbConfig = (opt) =>
 {
     opt.UseSqlServer(connectionString);
     // opt.EnableSensitiveDataLogging(true);
 };
+
+// Database context registration
 builder.Services.AddDbContext<ContentContext>(dbConfig);
 builder.Services.AddDbContextFactory<LanguageSupportContext>(dbConfig);
 builder.Services.AddDbContext<LanguageSupportContext>(dbConfig);
@@ -25,13 +39,13 @@ builder.Services.AddPooledDbContextFactory<TeksContext>(dbConfig, 3000);
 builder.Services.AddDbContextFactory<ElpsContext>(dbConfig);
 builder.Services.AddDbContext<ElpsContext>(dbConfig);
 
-// service registration
+// Service registration
 builder.Services.AddScoped<IContentService, ContentService>();
 builder.Services.AddScoped<ITeksService, TeksService>();
 builder.Services.AddScoped<ILanguageSupportService, LanguageSupportService>();
 builder.Services.AddScoped<IElpsService, ElpsService>();
 builder.Services.AddScoped<ITranslatorService, TranslatorService>();
-
+builder.Services.AddScoped<IExternalContentService, ExternalContentService>();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
@@ -124,7 +138,6 @@ app.MapPost(
     )
     .WithOpenApi();
 
-// TODO - finish writing this and deploy!
 app.MapGet(
         "/supports/elps",
         ElpsSupportResponse (
@@ -140,11 +153,24 @@ app.MapGet(
     .WithOpenApi();
 
 app.MapGet(
-    "/content/source",
-    () =>
-    {
-        PdfDocumentParser.ParsePdfDocument();
-    }
-);
+        "/content/external",
+        async (
+            [FromQuery(Name = "pdfUrl")] string pdfUrl,
+            [FromQuery(Name = "pages")] string pages,
+            IExternalContentService externalContentService
+        ) =>
+        {
+            return await externalContentService.AnalyzePdfDocument(pdfUrl, pages);
+        }
+    )
+    .WithOpenApi();
+
+// app.MapGet(
+//     "/content/source",
+//     () =>
+//     {
+//         PdfDocumentParser.ParsePdfDocument();
+//     }
+// );
 
 app.Run();
